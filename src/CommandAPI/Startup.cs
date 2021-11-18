99dc96cfa07d297427ed3bfcee2487d0bd3dcddc
Samuel <sami224491@gmail.com>
2021-11-18 10:45:20 +0300

    environment variables update

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -5,19 +5,36 @@ using System.Threading.Tasks;
 using CommandAPI.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
+using Microsoft.EntityFrameworkCore;
 namespace CommandAPI
 {
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        
+        
+        public IConfiguration Configuration{get;}
+        
+        public Startup(IConfiguration configuration){
+            Configuration=configuration;
+        }
+        
+        
+        
+        
+        
+        
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CommandContext>(optionsAction=>optionsAction.UseNpgsql
+            (Configuration.GetConnectionString("postgreSqlConnection")));
             services.AddControllers();
-            services.AddScoped<ICommandAPIRepo,MockCommandAPIRepo>();
+            // services.AddScoped<ICommandAPIRepo,MockCommandAPIRepo>();
+            services.AddScoped<ICommandAPIRepo,SqlCommandAPIRepo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
